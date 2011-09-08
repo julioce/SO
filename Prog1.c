2010b35ada3afcb54323121156deb16d88dbd9d4
Júlio Bueno <julioce@gmail.com>
2011-09-08 12:45:55 -0300

    Término da implementação       -Adição da resposta do Filho ao Pai retornando o valor j.        (Modifiquei o valor de incremento do for de i++ para j++ para evitar o loop infinito        Acredito que haja um problema na especificação do trabalho. Realmente não sei.)
          -Respostas do que acontece com o execl.

--- Prog1.c
+++ Prog1.c
@@ -8,15 +8,15 @@ int main(void){
 	int	status, id, j;
 	char mensagem[255];
 	
-	//Insira um comando para pegar o PID do processo corrente e mostre na tela da console.
-	printf("Processo Corrente - %i\n\n", getpid());
-	
-	//Cria o canal de comunicação inter-processos
+	//Cria os canais de comunicação inter-processual
 	int canalPai[2];
 	int canalFilho[2];
 	pipe(canalPai);
 	pipe(canalFilho);
 	
+	//Insira um comando para pegar o PID do processo corrente e mostre na tela da console.
+	printf("Processo Corrente - %i\n\n", getpid());
+
 	id = fork();
 	
 	if (id != 0){
@@ -33,18 +33,25 @@ int main(void){
 		printf("Mensagem enviada ao Filho: %s\n", mensagem);
 		
 		//Aguarde a resposta do processo filho
-		wait(&status);
+		wait(&canalFilho[1]);
 		
 		//Mostre na tela o texto recebido do processo filho
 		read(canalFilho[0], mensagem, sizeof(mensagem));//Lê a mensagem no canal de leitura
 		close(canalFilho[1]); //Fecha o canal de escrita
-		printf("Mensagem enviada pelo processo Filho - %s\n" , mensagem);
+		printf("1a Mensagem enviada pelo processo Filho - %s\n" , mensagem);
 		
-		/*
-		***** Aguarde mensagem do filho e mostre o texto recebido
-		***** Aguarde o término do processo filho
-		***** Informe na tela que o filho terminou e que o processo pai também vai encerrar
-		 */
+		//Aguarde mensagem do filho e mostre o texto recebido
+		wait(&canalFilho[1]);
+		read(canalFilho[0], mensagem, sizeof(mensagem));//Lê a mensagem no canal de leitura
+		close(canalFilho[1]); //Fecha o canal de escrita
+		printf("2a Mensagem enviada pelo processo Filho - %s\n" , mensagem);
+		
+		//Aguarde o término do processo filho
+		wait(&status);
+
+		//Informe na tela que o filho terminou e que o processo pai também vai encerrar
+		printf("O Processo Filho terminou e o pai também se encerrará.");
+		
 	}else{
 		//Faça com que o processo filho execute este trecho de código
 		//Mostre na tela o PID do processo corrente e do processo pai
@@ -60,14 +67,24 @@ int main(void){
 		close(canalFilho[0]); //Fecha o canal de leitura
 		write(canalFilho[1], mensagem, 255); //Escreve a mensagem no canal de escrita
 		
+		
+		//Execute o comando “for” abaixo
+		for (j = 0; j <= 10000; j++);
+			
+		//Envie mensagem ao processo pai com o valor final de “j”
+		sprintf(mensagem, "%i", j, 10);
+		close(canalFilho[0]); //Fecha o canal de leitura
+		write(canalFilho[1], mensagem, 255); //Escreve a mensagem no canal de escrita
+
+		//Execute o comando abaixo e responda às perguntas
+		execl("/Bin/ls", "ls", NULL);
 		/*
-		***** Execute o comando “for” abaixo
-		for (j = 0; j <= 10000; i++);
-			***** Envie mensagem ao processo pai com o valor final de “j”
-			***** Execute o comando abaixo e responda às perguntas
-			execl(“/Bin/ls”, “ls”, NULL);
 			***** O que acontece após este comando?
+			O processo filho continua o seu fluxo de código e termina sua execução,
+			retornando ao processo o seu status de terminado.
+
 			***** O que pode acontecer se o comando “execl” falhar?
+			Não é exibido nada nada tela e portanto o processo é encerrado com erro.
 		 */
 		exit(0);
 	}

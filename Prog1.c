13bf200b844c8612aa2f28eb67bda3b95ba96000
Júlio Bueno <julioce@gmail.com>
2011-09-16 01:19:38 -0300

    Modifcações em Prog1      -Alteração final do Prog1      -Limpeza de arquivos temporários

--- Prog1.c
+++ Prog1.c
@@ -1,80 +1,117 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <sys/wait.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <mqueue.h>
+
+#define MSG_SIZE 256
+#define MAX_MSG_SIZE 10000
+#define MSGQOBJ_NAME "/IPC_CHANNEL"
 
 int main(void){
 	int	status, id, j;
-	char mensagem[255];
-	
-	//Cria os canais de comunicação inter-processual
-	int canalPai[2];
-	int canalFilho[2];
-	pipe(canalPai);
-	pipe(canalFilho);
+
+	//Cria as estruturas de IPC
+	mqd_t msgq_id;
+	unsigned int sender;
+	char msgcontent[MSG_SIZE];
+	char msgcontentRCV[MAX_MSG_SIZE];
+	struct mq_attr msgq_attr;
+	int msgsz;
 	
 	//Insira um comando para pegar o PID do processo corrente e mostre na tela da console.
 	printf("Processo Corrente - %i\n\n", getpid());
-
+	
 	id = fork();
 	
 	if (id != 0){
 		//Faça com que o processo pai execute este trecho de código
 		//Mostre na console o PID do processo pai e do processo filho
 		printf("Sou o processo Pai de PID %i e tenho o processo Filho de PID %i\n", getpid(), id);
+
 		
+		//Abre o queue
+		msgq_id = mq_open(MSGQOBJ_NAME, O_RDWR | O_CREAT | O_EXCL, S_IRWXU | S_IRWXG, NULL);
+		if (msgq_id < 0) {
+		    msgq_id = mq_open(MSGQOBJ_NAME, O_RDWR);
+		}
+
+
 		//Monte uma mensagem e a envie para o processo filho
-		strcpy(mensagem, "Olá processo Filho!");
-		close(canalPai[0]); //Fecha o canal de leitura
-		write(canalPai[1], mensagem, 255); //Escreve a mensagem no canal de escrita
+		strcpy(msgcontent, "Olá processo Filho!");
+		mq_send(msgq_id, msgcontent, strlen(msgcontent)+1, 10);
+
 		
 		//Mostre na tela o texto da mensagem enviada
-		printf("Mensagem enviada ao Filho: %s\n", mensagem);
-		
-		//Aguarde a resposta do processo filho
-		wait(&canalFilho[1]);
-		
-		//Mostre na tela o texto recebido do processo filho
-		read(canalFilho[0], mensagem, sizeof(mensagem));//Lê a mensagem no canal de leitura
-		close(canalFilho[1]); //Fecha o canal de escrita
-		printf("1a Mensagem enviada pelo processo Filho - %s\n" , mensagem);
-		
-		//Aguarde mensagem do filho e mostre o texto recebido
-		wait(&canalFilho[1]);
-		read(canalFilho[0], mensagem, sizeof(mensagem));//Lê a mensagem no canal de leitura
-		close(canalFilho[1]); //Fecha o canal de escrita
-		printf("2a Mensagem enviada pelo processo Filho - %s\n" , mensagem);
-		
+		printf("Mensagem enviada ao Filho: %s\n", msgcontent);	
+
+
 		//Aguarde o término do processo filho
 		wait(&status);
-
+		
+		
+		//Aguarde a resposta do processo filho
+		msgsz = mq_receive(msgq_id, msgcontentRCV, MAX_MSG_SIZE, &sender);
+		if (msgsz == -1) {
+			perror("In mq_receive()");
+			exit(1);
+		}
+		printf("1a Mensagem enviada pelo filho - %s\n", msgcontentRCV);
+		
+		
+		//Aguarde mensagem do filho e mostre o texto recebido
+		msgsz = mq_receive(msgq_id, msgcontentRCV, MAX_MSG_SIZE, &sender);
+		if (msgsz == -1) {
+			perror("In mq_receive()");
+			exit(1);
+		}
+		printf("2a Mensagem enviada pelo filho - %s\n", msgcontentRCV);
+		
+		
+		mq_close(msgq_id);
+		
 		//Informe na tela que o filho terminou e que o processo pai também vai encerrar
-		printf("O Processo Filho terminou e o pai também se encerrará.");
+		printf("O Processo Filho terminou e o pai também se encerrará.\n");
 		exit(0);
 	}else{
 		//Faça com que o processo filho execute este trecho de código
 		//Mostre na tela o PID do processo corrente e do processo pai
 		printf("Sou o processo de PID %i e tenho o Processo Pai de PID %i\n", getpid(), getppid());
 		
+		//Abre o message queue
+		msgq_id = mq_open(MSGQOBJ_NAME, O_RDWR);
+		if (msgq_id == (mqd_t)-1) {
+			perror("In mq_open()");
+			exit(1);
+		}
+
+		
 		//Aguarde a mensagem do processo pai e ao receber mostre o texto na tela
-        wait(&canalPai[1]);
-		read(canalPai[0], mensagem, sizeof(mensagem));//Lê a mensagem no canal de leitura
-		close(canalPai[1]); //Fecha o canal de escrita
-		printf("Mensagem enviada pelo processo Pai - %s\n" , mensagem);
+		mq_getattr(msgq_id, &msgq_attr);
+		msgsz = mq_receive(msgq_id, msgcontentRCV, MAX_MSG_SIZE, &sender);
+		if (msgsz == -1) {
+			perror("In mq_receive()");
+			exit(1);
+		}
+		printf("Mensagem enviada pelo pai - %s\n", msgcontentRCV);
+		
 		
 		//Envie uma mensagem resposta ao pai
-		strcpy(mensagem, "Olá processo Pai!");
-		close(canalFilho[0]); //Fecha o canal de leitura
-		write(canalFilho[1], mensagem, 255); //Escreve a mensagem no canal de escrita
+		strcpy(msgcontent, "Olá processo Pai!");
+		mq_send(msgq_id, msgcontent, strlen(msgcontent)+1, 10);
+		
 		
 		//Execute o comando “for” abaixo
 		for (j = 0; j <= 10000; j++);
-			
+
+		
 		//Envie mensagem ao processo pai com o valor final de “j”
-		sprintf(mensagem, "j=%i", j, 10);
-		close(canalFilho[0]); //Fecha o canal de leitura
-		write(canalFilho[1], mensagem, 255); //Escreve a mensagem no canal de escrita
+		sprintf(msgcontent, "j=%i", j);
+		mq_send(msgq_id, msgcontent, strlen(msgcontent)+1, 10); 
+
 
 		//Execute o comando abaixo e responda às perguntas
 		execl("/bin/ls", "ls", NULL);
@@ -90,7 +127,8 @@ int main(void){
 			***** O que pode acontecer se o comando “execl” falhar?
 			Como o ls é um processo independente do Prog1, quando o comando execl falha 
 			o Prog1 continua sua execução normalmente. 
-		 */
+		*/
+		mq_close(msgq_id);
 		exit(0);
 	}
 	

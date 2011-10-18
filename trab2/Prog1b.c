c55a6569e40744e9d4188160f0c30c241a1b6b4b
Júlio Bueno <julioce@gmail.com>
2011-10-18 20:24:49 -0200

    Desenvolvimento do Prog1:    -Prog1 foi dividido em duas abordagens       *Prog1 utiliza threads mas não possui memória compartilhada       *Prog1b utiliza threads e memória compartilhada (como no Prog3c em Trab1)    -Prog1 teve a melhoria de criação e gerenciamento das linhas de cada Thread dinâmico através de THREADS    -Prog1b também está dinâmico e agora não contabiliza o tempo de criação da memória compartilhada

--- trab2/Prog1b.c
+++ trab2/Prog1b.c
@@ -3,15 +3,32 @@
 #include <unistd.h>
 #include <math.h>
 #include <sys/timeb.h>
+
+#include <sys/wait.h>
+
+#include <sys/types.h>
+#include <sys/ipc.h> 
+#include <sys/shm.h>
+
 #include <pthread.h>
 
 #define INF 0x33333333
 #define THREADS 4
 
-int **matriz, *produtoInterno, i, j, menor_i, maior_i, k, m, menor, maior, parte[THREADS], returnThread[THREADS];
-double soma, soma_desvio, desvio_padrao, tempo_execucao;
-struct timeb inicio_execucao, fim_execucao;
 pthread_t thread[THREADS];
+int parte[THREADS];
+int returnThread[THREADS];
+int i, j, m, k, somatorio, **matriz, *produtoInterno, *pids, id, status;
+double soma_desvio, desvio_padrao, tempo_execucao;
+int shmid[10];
+//Cria estrutura de compartilhamento de memória
+struct shared compartilhado;
+
+struct shared{
+	double *variancia;
+	int shmid, *produtoInterno, *produtoInterno_compartilhado, *menor_i, *maior_i, *menor, *maior;
+	key_t key;
+};
 
 int **aloca_matriz(int m, int k) {
 	//ponteiro para a matriz e variável de iteração
@@ -33,7 +50,7 @@ int **aloca_matriz(int m, int k) {
 	//Aloca as colunas
 	for( i = 0; i < m; i++ ){
 		v[i] = (int*) calloc (k, sizeof(int));
-		if(v[i] == NULL){
+		if (v[i] == NULL){
 			printf ("\n\nErro: Memória insuficiente!\n\n");
 			exit(1);
 		}
@@ -53,17 +70,17 @@ int **free_matriz(int m, int k, int **v){
 	
 	//Verifica parâmetros
 	if(m < 1 || k < 1){
-		printf("\n\nErro: Parâmetro invalido!\n\n");
-		return(v);
+		printf ("\n\nErro: Parâmetro invalido!\n\n");
+		return (v);
 	}
 	
 	//Libera as linhas da matriz
 	for(i=0; i<m; i++){
-		free(v[i]);
+		free (v[i]);
 	}
 	
 	//Libera a matriz
-	free(v);
+	free (v);
 	return (NULL);
 }
 
@@ -90,6 +107,11 @@ int *aloca_vetor(int m){
 }
 
 
+int max(int a, int b){
+	if(a >= b){return a;}
+	else{return b;}
+}
+
 void *calculaProdutoInterno(void *arg){
 	int i,j, somatorio, *pos;
 	pos = (int *) arg;
@@ -112,14 +134,16 @@ void *calculaProdutoInterno(void *arg){
 			somatorio += matriz[i][j] * matriz[i][j];
 		}
 		
-		//Armazena o PI(i) e soma para cálculo de média
-		produtoInterno[i] = somatorio;
-		soma += produtoInterno[i];
+		//Armazena o PI(i) e soma para cálculo de média para o desvio padrão
+		compartilhado.produtoInterno_compartilhado[i] = somatorio;
+		*compartilhado.variancia += compartilhado.produtoInterno_compartilhado[i];
 	}
 }
 
-
 int main(void){
+	struct timeb inicio_execucao, fim_execucao;
+	srand((unsigned)time(NULL));
+	
 	
 	//Recebe os valores iniciais de m e k
 	printf("Defina o número de linhas  -> m = ");
@@ -127,29 +151,76 @@ int main(void){
 	printf("Defina o número de colunas -> k = ");
 	scanf("%i", &k);
 	
-	srand((unsigned)time(NULL));
 	
 	while( m!= 0 && k!=0 ){
+		//Cria o compartilhamento de memória
+		for(i=0; i<6; i++){
+			if(i == 0){
+				if( (shmid[i] = shmget(getpid(), max(m, k)*sizeof(int), IPC_CREAT | SHM_W | SHM_R)) < 0 ){
+					if((shmid[i] = shmget((unsigned)time(NULL), max(m, k)*sizeof(int), IPC_CREAT | SHM_W | SHM_R)) < 0){
+						perror("shmget 1");
+						exit(1);
+					}
+				}
+			}
+			else if(i > 0 && i < 5){
+				if((shmid[i] = shmget(getpid()+i, sizeof(int), IPC_CREAT | SHM_W | SHM_R)) < 0){
+					perror("shmget 2");
+					exit(1);
+				}
+			}
+			else{
+				if((shmid[i] = shmget(getpid()+5, sizeof(double), IPC_CREAT | SHM_W | SHM_R)) < 0){
+					perror("shmget 3");
+					exit(1);
+				}
+			}
+		}
+		if((compartilhado.produtoInterno_compartilhado = shmat(shmid[0], NULL, 0)) == (int *) -1){
+			perror("shmat produtointerno");
+			exit(1);
+		}
+		if((compartilhado.menor = shmat(shmid[1] , NULL, 0)) == (int *) -1){
+			perror("shmat menor");
+			exit(1);
+		}
+		if((compartilhado.maior = shmat(shmid[2], NULL, 0)) == (int *) -1){
+			perror("shmat maior");
+			exit(1);
+		}
+		if((compartilhado.menor_i = shmat(shmid[3], NULL, 0)) == (int *) -1){
+			perror("shmat menor_i");
+			exit(1);
+		}
+		if((compartilhado.maior_i = shmat(shmid[4], NULL, 0)) == (int *) -1){
+			perror("shmat maior i");
+			exit(1);
+		}
+		if((compartilhado.variancia = shmat(shmid[5], NULL, 0)) == (double *) -1){
+			perror("shmat variancia");
+			exit(1);
+		}
+		
+		
 		//Inicializa outros valores da iteração
-		menor = INF;
-		maior = -INF;
-		menor_i = 0;
-		maior_i = 0;
-		soma = 0;
+		*compartilhado.menor = INF;
+		*compartilhado.maior = -INF;
+		*compartilhado.variancia = 0;
 		soma_desvio = 0;
 		desvio_padrao = 0;
 		
 		//Inicia a contagem do tempo de execução
 		ftime(&inicio_execucao);
 		
-		//Aloca a matriz e vetor de Produto Interno
+		//Aloca a matriz Principal, vetor de Produto Interno e de PIDs
 		printf("\nMontando a matriz... ");
 		matriz = aloca_matriz(m, k);
-		produtoInterno = aloca_vetor(m);
+		compartilhado.produtoInterno = aloca_vetor(m);
+		pids = aloca_vetor(m);
 		printf("Concluído!\n\n");
 		
 		
-		//Gera a matriz
+		//Insere na matriz
 		printf("Inserindo valores na Matriz... ");
 		for(i=0; i<m; i++){
 			for(j=0; j<k; j++){
@@ -184,16 +255,16 @@ int main(void){
 		
 		//Calcula o desvio padrão
 		for(i=0; i<m; i++){
-			soma_desvio += pow(produtoInterno[i]-(soma/m), 2);
+			soma_desvio += pow(compartilhado.produtoInterno_compartilhado[i]-(*compartilhado.variancia/m), 2);
 			
 			//Detecta o maior e menor
-			if(produtoInterno[i] <= menor){
-				menor = produtoInterno[i];
-				menor_i = i+1;
+			if(compartilhado.produtoInterno_compartilhado[i] <= *compartilhado.menor){
+				*compartilhado.menor = compartilhado.produtoInterno_compartilhado[i];
+				*compartilhado.menor_i = i+1;
 			}
-			if(produtoInterno[i] >= maior){
-				maior = produtoInterno[i];
-				maior_i = i+1;
+			if(compartilhado.produtoInterno_compartilhado[i] >= *compartilhado.maior){
+				*compartilhado.maior = compartilhado.produtoInterno_compartilhado[i];
+				*compartilhado.maior_i = i+1;
 			}
 		}
 		desvio_padrao = sqrt(soma_desvio/m);
@@ -206,15 +277,15 @@ int main(void){
 		
 		//Libera a matriz
 		free_matriz(m, k, matriz);
-		free(produtoInterno);
 		
 		
 		//Exibe os valores resultantes
 		printf("--------------------------Valores Aferidos---------------------------\n");
-		printf("Menor valor = %i (m=%i) e Maior valor = %i (m=%i)\n", menor, menor_i, maior, maior_i);
+		printf("Menor valor = %i (i=%i) e Maior valor = %i (i=%i)\n", *compartilhado.menor, *compartilhado.menor_i, *compartilhado.maior, *compartilhado.maior_i);
 		printf("Desvio Padrão = %f\n", desvio_padrao);
 		printf("Tempo de execução = %.3f segundos\n", tempo_execucao);
 		printf("---------------------------------------------------------------------\n");
+		
 		
 		//Recebe os valores de m e k para nova iteração
 		printf("Montando a matriz da nova iteração\n\n");
@@ -226,4 +297,3 @@ int main(void){
 	
 	exit(0);
 }
-
